e63b10b2991dcadc66d7d8840bcd0d415e4f0b16
Nilav <nilavtalukdar06@gmail.com>
2025-03-03 10:01:45 +0530

    Db is connected

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,4 +1,11 @@
+import { connectDb } from "@/lib/config/db";
 import { NextRequest, NextResponse } from "next/server";
+
+const loadDb = async () => {
+  connectDb();
+};
+
+loadDb();
 
 export async function GET(reuqest) {
   return NextResponse.json({
