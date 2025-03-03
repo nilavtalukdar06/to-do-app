b9f1de8dd1a9e8756b0c6b3aa6283e19fcffed8d
Nilav <nilavtalukdar06@gmail.com>
2025-03-03 10:35:13 +0530

    Created create route

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,4 +1,5 @@
 import { connectDb } from "@/lib/config/db";
+import { taskModel } from "@/lib/models/task";
 import { NextRequest, NextResponse } from "next/server";
 
 const loadDb = async () => {
@@ -10,5 +11,21 @@ loadDb();
 export async function GET(reuqest) {
   return NextResponse.json({
     msg: "GET Method",
+  });
+}
+
+export async function POST(request) {
+  const { title, description } = await request.json();
+  try {
+    await taskModel.create({
+      title,
+      description,
+    });
+    console.log("Data created successfully");
+  } catch (error) {
+    console.error(`Error creating data, error: ${error}`);
+  }
+  return NextResponse.json({
+    msg: "TODO Created",
   });
 }
