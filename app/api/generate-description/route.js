b0789cd692e73451cce9d8708954349024cc458a
Nilav <nilavtalukdar06@gmail.com>
2025-05-04 20:11:49 +0530

    Added api rate limiting to the ai generation api

--- app/api/generate-description/route.js
+++ app/api/generate-description/route.js
@@ -1,13 +1,37 @@
 import { NextResponse } from "next/server";
 import main from "@/utils/gemini/gemini";
+import arcjet, { tokenBucket } from "@arcjet/next";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["userId"],
+  rules: [
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
 
 export async function POST(request) {
   try {
-    const { title } = await request.json();
-    const response = await main(title);
-    if (!response) {
-      throw new Error("Ai didn't return any response");
+    const { title, user_id } = await request.json();
+    if (!title || !user_id) {
+      throw new Error("Incomplete fields");
     }
+    const userId = user_id;
+    const decision = await aj.protect(request, { userId, requested: 5 });
+    console.log("Arcjet decision", decision);
+
+    if (decision.isDenied()) {
+      return NextResponse.json(
+        { error: "Too Many Requests", reason: decision.reason },
+        { status: 429 }
+      );
+    }
+    const response = await main(title);
     return NextResponse.json(
       {
         description: response,
