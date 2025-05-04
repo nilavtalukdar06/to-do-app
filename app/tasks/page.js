8e14644e673bc3c7bb076420126c6114aa4d8281
Nilav <nilavtalukdar06@gmail.com>
2025-05-04 15:43:23 +0530

    Added table

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,8 +1,12 @@
+import { TaskTable } from "@/components/TaskTable";
+
 export default function Tasks() {
   return (
     <section className="min-h-screen max-w-screen overflow-x-hidden relative">
-      <div className="my-10 md:my-20 max-w-2xl mx-auto p-4">
-        <div className="w-full flex justify-center items-center">Task Page</div>
+      <div className="my-10 md:my-20 max-w-4xl mx-auto p-4">
+        <div className="w-full flex justify-center items-center">
+          <TaskTable />
+        </div>
       </div>
     </section>
   );
